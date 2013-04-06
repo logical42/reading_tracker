d51aee2c4cbd6408a94f8da59c14b7af98b6c4ec
logical42 <han@thirstlabs.com>
2013-04-06 13:55:45 -0700

    options testing

--- spec/models/tracker_spec.rb
+++ spec/models/tracker_spec.rb
@@ -5,12 +5,26 @@ describe "Tracker" do
       Tracker.rspec_works.should eq(true)
     end
   end
+  context "initialization" do
+    it "should take an optional hash when initializing" do 
+      expect { Tracker.new(url: "asdf") }.to_not raise_error
+    end
+    it "should not raise an error when initializing without any options" do 
+      expect { Tracker.new }.to_not raise_error
+    end
+  end
   context "connect to web" do
     before(:each) do 
       @tracker = Tracker.new
+      @default_url = "http://classics.mit.edu/Tzu/artwar.1b.txt"
     end
     it "should have a default url" do
       @tracker.url.should_not eq("")
     end
+
+    it "should default the url to art of war.1b" do
+      @tracker.url.should eq(@default_url)
+    end
+
   end
 end
