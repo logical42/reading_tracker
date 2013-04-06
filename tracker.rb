152852928ce9882d751bc6d95dba1d7909efac25
logical42 <han@thirstlabs.com>
2013-04-06 14:27:01 -0700

    add initialization methods

--- tracker.rb
+++ tracker.rb
@@ -1,10 +1,23 @@
 class Tracker
+  attr_accessor :url, :average_prose, :average_screen, :user_wpm
+
+  ### CLASS METHODS 
+  def initialize(options={})
+    self.url = options[:url] || "http://classics.mit.edu/Tzu/artwar.1b.txt"
+
+  end
 
   def self.rspec_works
     true
   end
 
-  def url
-  	"not blank"
-  end
+  ### END CLASS METHODS
+
+
+
+  ### INSTANCE METHODS
+
+
+
+  ### END INSTANCE METHODS
 end
