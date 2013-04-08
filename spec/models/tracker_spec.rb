a1a62a0aaadfc9d1cfd8b46e73bab65a761a9906
captconrado <ottey001@gmail.com>
2013-04-08 00:02:56 -0700

    added the wpm spec

--- spec/models/tracker_spec.rb
+++ spec/models/tracker_spec.rb
@@ -1,4 +1,5 @@
 require File.expand_path("../../../tracker", __FILE__)
+
 describe "Tracker" do
   context "making sure rspec works" do
     it "should return true when the method rspec_works is called" do
@@ -41,16 +42,6 @@ describe "Tracker" do
     it "should be able to choose a random passage and it should be greater than the min word length" do
       tracker = Tracker.new(min_word_count: 100)
       tracker.book.pick_random_passage.split(" ").count.should > tracker.min_word_count
-    end
-  end
-  context "words per minute calculation" do
-    before(:each) do
-      tracker = Tracker.new
-      @default_url = "http://classics.mit.edu/Confucius/doctmean.1b.txt"
-    end
-    it "should accuratly calculate the words per minute of a passage" do
-      tracker = Tracker.new
-      tracker.book.parse_passages.last.size.should == 462 
     end
   end
 end
---
+++ spec/models/words_per_min_spec.rb
@@ -0,0 +1,14 @@
+require File.expand_path("../../../tracker", __FILE__)
+
+describe "WPMCalculator" do
+	context "words per minute calculation" do
+    before(:each) do
+      tracker = Tracker.new
+      @default_url = "http://classics.mit.edu/Confucius/doctmean.1b.txt"
+    end
+    it "should count the words in the passage" do
+      tracker = Tracker.new
+      tracker.book.parse_passages.last.size.should == 462 
+    end
+  end
+end
